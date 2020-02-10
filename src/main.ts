f886b4863368b0d1f9db71bef7f02ec795803e6d
speckyguy <sarunss31@gmail.com>
2020-02-10 12:46:07 +0530

    the commit to be changed

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Default from '@/layout/Default.vue'
+import NoHeader from '@/layout/NoHeader.vue'
+Vue.component('default-layout', Default)
+Vue.component('noHeader-layout', NoHeader)
 new Vue({
   router,
   store,
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomePage from '@/components/HomePage.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import SearchPage from '@/views/SearchPage.vue'
 
 Vue.use(VueRouter)
 const routes = [
-  { path: '/home', component: HomePage }
+  { path: '/', redirect: '/search' },
+  { path: '/search', meta: { layout: 'noHeader' }, component: SearchPage },
+  { path: '/home', component: Home },
+  { path: '/about', component: About }
 ]
 
 const router = new VueRouter({
